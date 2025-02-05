a0a2be505a41fe7da124cc3dd44ec0b0e82b76f2
Vaniljkola <130172331+Vaniljkola@users.noreply.github.com>
2025-02-05 15:50:04 +0100

    Update floating_point.py

--- rand/floating_point.py
+++ rand/floating_point.py
@@ -1,7 +1,14 @@
 a = 0.2 + 0.4
 print("value:",a)
 
+
 if a == 0.6:
 	print(True)
 else:
 	print(False)
+
+
+# Svar: a är 0.60000001 och False då vissa decimaler inte kan representeras exakt binärt.
+
+# Matematiskt är det likt 1 / 3 då det blir 1.333333333333333.... 
+# För att verifiera 
